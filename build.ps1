996ec5ccc7dd7dec790d16d4d2d3838e572de78f
Anthony Conrad <parithon@outlook.com>
2022-09-20 15:43:15 -0700

    Updated build script

--- build.ps1
+++ build.ps1
@@ -1,2 +1,3 @@
+$ModuleVersion = "1.0.3"
 $Path = "../Terraform/terraform.d/plugins/registry.terraform.io/taliesins/hyperv"
-go build -o "$Path/1.0.3/windows_amd64/terraform-provider-hyperv_1.0.3.exe"
+go build -o "$Path/$ModuleVersion/windows_amd64/terraform-provider-hyperv_${ModuleVersion}.exe"
